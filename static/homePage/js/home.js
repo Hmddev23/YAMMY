ada53dd52ccaa4a29e2d220955c2c1f8efb959f8
Hmddev23 <hamid.maira709@gmail.com>
2024-04-12 22:50:34 +0000

    push 15

--- static/homePage/js/home.js
+++ static/homePage/js/home.js
@@ -5,16 +5,16 @@ HOME JS
 =======
 */
 
-const carousel = document.querySelector('.carousel');
+/* const carousel = document.querySelector('.carousel');
 const arrowIcons = document.querySelectorAll('.arrowIcon');
 const firstImg = carousel.querySelectorAll('img')[0];
-let isDragStart = false, prevPageX, prevScrollLeft;
 let firstImgWidth = firstImg.clientWidth + 14;
+let isDragStart = false, prevPageX, prevScrollLeft;
 
 arrowIcons.forEach((icon) => {
   icon.addEventListener('click', () => {
-    // console.log(icon);
-    carousel.scrollLeft += icon.id == 'left' ? -firstImgWidth : firstImgWidth;
+    console.log(icon);
+    carousel.scrollLeft += icon.id == 'arrow-left' ? -firstImgWidth : firstImgWidth;
   });
 });
 
@@ -39,4 +39,23 @@ const dragStop = () => {
 
 carousel.addEventListener('mousedown', dragStart);
 carousel.addEventListener('mousemove', dragging);
-carousel.addEventListener('mouseup', dragStop);
+carousel.addEventListener('mouseup', dragStop); */
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const prev = document.getElementById('prev-btn');
+  const next = document.getElementById('next-btn');
+  const list = document.getElementById('carousel');
+
+  const itemWidth = 150;
+  const padding = 10;
+
+  prev.addEventListener('click',()=>{
+    list.scrollLeft -= itemWidth + padding;
+  });
+
+  next.addEventListener('click',()=>{
+    list.scrollLeft += itemWidth + padding;
+  });
+});
